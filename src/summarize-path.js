72e7550a0dbb90f2952da848cce806512ccc1a00
rianbotha <hello@rian.co.za>
2019-11-19 11:47:52 +0200

    Allow arrays of paths

--- src/summarize-path.js
+++ src/summarize-path.js
@@ -1,17 +1,20 @@
-const summarizePath = (report, path, name) => {
+const summarizePath = (report, paths, name) => {
   let lines = 0;
   let coveredLines = 0;
+  const packages = paths;
 
-  const package = path.replace(/\//g, '.');
-  const sections = report.coverage.project[0].package.filter(entry => entry.ATTR.name.indexOf(package) === 0);
-  sections.forEach(section => {
-    lines += parseInt(section.metrics[0].ATTR.statements, 10);
-    coveredLines += parseInt(section.metrics[0].ATTR.coveredstatements, 10);
+  packages.forEach(path => {
+    const package = path.replace(/\//g, '.');
+    const sections = report.coverage.project[0].package.filter(entry => entry.ATTR.name.indexOf(package) === 0);
+    sections.forEach(section => {
+      lines += parseInt(section.metrics[0].ATTR.statements, 10);
+      coveredLines += parseInt(section.metrics[0].ATTR.coveredstatements, 10);
+    });
   });
 
   const coveredPercent = `${Math.round(coveredLines / lines * 100 * 100) / 100}%`
 
-  return [name || path, coveredLines, lines, coveredPercent];
+  return [name || path[0], coveredLines, lines, coveredPercent];
 };
 
 module.exports = summarizePath;
