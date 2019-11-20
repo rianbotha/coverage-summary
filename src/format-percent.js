0f3e0c3d74a860aba34dd1f0174ea5439f64bf0e
rianbotha <hello@rian.co.za>
2019-11-20 10:23:54 +0200

    Make 0% work

--- src/format-percent.js
+++ src/format-percent.js
@@ -1,6 +1,7 @@
 const formatThreshold = require('./format-threshold');
 
 const formatPercent = (number, total) => {
+  if (number === 0 || total === 0) return formatThreshold('0%', 0);
   const percent = Math.round(number / total * 100 * 100) / 100;
   return formatThreshold(percent + '%', percent);
 };
